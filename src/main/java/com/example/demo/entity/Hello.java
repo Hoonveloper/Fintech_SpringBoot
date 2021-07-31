d0965b98447251a72243ffa22b3b5f5c19eecbf9
Your Name <you@example.com>
2021-07-31 12:41:23 +0900

    hello entity

--- src/main/java/com/example/demo/entity/Hello.java
+++ src/main/java/com/example/demo/entity/Hello.java
@@ -1,10 +1,8 @@
 package com.example.demo.entity;
 
+import lombok.Data;
+
+@Data
 public class Hello {
-    String name;
-    public Hello(String name){
-        this.name=name;
-
-
-    }
+    final String name;
 }
